4f76bbdad8e6fa319d82686f2254128a7d9b84ad
Shawn Jiang <shawn.x.jiang@gmail.com>
2021-09-06 22:37:27 +0800

    update the config

--- configure.sh
+++ configure.sh
@@ -1,5 +1,5 @@
 
-/localhome/hja40/miniconda3/envs/ANCSH
+CONDA_ENV_DIR=/localhome/hja40/miniconda3/envs/ANCSH
 
-export PATH="/usr/local/cuda-11.0/bin/:$PATH"
-export LD_LIBRARY_PATH="/usr/local/cuda-11.0/lib64/:$CONDA_ENV_DIR/lib/python3.6/site-packages/tensorflow/:$LD_LIBRARY_PATH"
+export PATH="/localhome/hja40/Desktop/cuda-9.0/bin/:$PATH"
+export LD_LIBRARY_PATH="/localhome/hja40/Desktop/cuda-9.0/lib64/:$CONDA_ENV_DIR/lib/python3.6/site-packages/tensorflow/:$LD_LIBRARY_PATH"
